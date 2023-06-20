4d8359b2af28e09ed9302fbdd8cad9d0e959943f
CHr0NiclEs-EM <eri.rivas@icloud.com>
2023-06-20 10:23:03 -0400

    création de class voiture et conducteur

--- Exercice4Voitures.py
+++ Exercice4Voitures.py
@@ -1,2 +1,16 @@
 # Exercice Créer une liste de 4 Voiture
 # Trouver un moyen pour informer quelle personne conduit quelle voiture ???
+
+###Création de la class Voiture:
+class Voiture:
+    def __init__(self, Vmarque, Vmodel, Vannee):
+        self.marque = Vmarque
+        self.model = Vmodel
+        self.annee = Vannee
+
+### Création de la class Conducteur:
+class Conducteur:
+    def __init__(self, nom, gender, age):
+        self.nom = nom
+        self.gender = gender
+        self.age = age
